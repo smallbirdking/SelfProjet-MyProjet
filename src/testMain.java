e1b99fcafc5d1cd47bf544935728c5b7d8b0539b
smallbirdking <869084711@qq.com>
2017-03-08 23:03:59 +0100

    test commit

---
+++ src/testMain.java
@@ -0,0 +1,10 @@
+/**
+ * Created by PC on 2017/3/8.
+ */
+public class testMain {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
